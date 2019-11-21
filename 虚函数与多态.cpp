abe9d8234c82cb0ce4275170dbda9bc950535643
hhaa186 <872733844@qq.com>
2019-11-21 16:40:27 +0800

    19-11-21

--- 虚函数与多态.cpp
+++ 虚函数与多态.cpp
@@ -17,5 +17,13 @@ class Shape{
 class Rectangle:public Shape{};
 class Ellipse:public Shape{};
 
-//////虚函数继承。。。///子类对象可以调用父类函数   ////Template Method 23种设计模式之一
+//////虚函数继承。。。///子类对象可以调用父类函数   ////Template Method（模板函数？Method，Java术语：函数） 23种设计模式之一
 //////
+
+
+//继承+复合关系下的构造和析构
+//一个类既有父类继承又有复合    待验证。
+//一个类既有子类继承又有复合   构造：复合->父类->子类。析构相反。
+
+//Delegation委托+继承
+//被委托类被继承，被继承的父类的子类也可以被委托
